9d76e1ec6c2b392f4493b759b659fc2b7e942d9f
Abraham R <abraham@waldenberginc.com>
2021-08-09 13:08:59 +0200

    Add logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import calculate from "./logic/calculator";
 
 class Calculator extends React.Component {
   constructor(props) {
@@ -18,69 +19,69 @@ class Calculator extends React.Component {
         </div>
         <div className="body-buttons">
           <div className="buttons-row-1">
-            <button type="button" className="col-1 ">
+            <button name="AC" type="button" className="col-1 ">
               AC
             </button>
-            <button type="button" className="col-2 ">
+            <button name="+/-" type="button" className="col-2 ">
               +/-
             </button>
-            <button type="button" className="col-3 ">
+            <button name="%" type="button" className="col-3 ">
               %
             </button>
-            <button type="button" className="col-4 orange">
+            <button name="/" type="button" className="col-4 orange">
               /
             </button>
           </div>
           <div className="buttons-row-2">
-            <button type="button" className="col-1 ">
+            <button name="7" type="button" className="col-1 ">
               7
             </button>
-            <button type="button" className="col-2 ">
+            <button name="8" type="button" className="col-2 ">
               8
             </button>
-            <button type="button" className="col-3 ">
+            <button name="9" type="button" className="col-3 ">
               9
             </button>
-            <button type="button" className="col-4 orange">
+            <button name="x" type="button" className="col-4 orange">
               x
             </button>
           </div>
           <div className="buttons-row-3">
-            <button type="button" className="col-1 ">
+            <button name="4" type="button" className="col-1 ">
               4
             </button>
-            <button type="button" className="col-2 ">
+            <button name="5" type="button" className="col-2 ">
               5
             </button>
-            <button type="button" className="col-3 ">
+            <button name="6" type="button" className="col-3 ">
               6
             </button>
-            <button type="button" className="col-4 orange">
+            <button name="-" type="button" className="col-4 orange">
               -
             </button>
           </div>
           <div className="buttons-row-4">
-            <button type="button" className="col-1 ">
+            <button name="1" type="button" className="col-1 ">
               1
             </button>
-            <button type="button" className="col-2 ">
+            <button name="2" type="button" className="col-2 ">
               2
             </button>
-            <button type="button" className="col-3 ">
+            <button name="3" type="button" className="col-3 ">
               3
             </button>
-            <button type="button" className="col-4 orange">
+            <button name="+" type="button" className="col-4 orange">
               +
             </button>
           </div>
           <div className="buttons-row-5">
-            <button type="button" className="col-1-2 ">
+            <button name="0" type="button" className="col-1-2 ">
               0
             </button>
-            <button type="button" className="col-3 ">
+            <button name="." type="button" className="col-3 ">
               .
             </button>
-            <button type="button" className="col-4 orange">
+            <button name="=" type="button" className="col-4 orange">
               =
             </button>
           </div>
